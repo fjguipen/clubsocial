373994341d1a0d89a700c02dbdb66c1af9ea19ac
JuanCC90 <juanmanuelcastrocanal@gmail.com>
2019-02-13 20:11:08 +0100

    reservando

---
+++ models/reserva.php
@@ -0,0 +1,33 @@
+<?
+
+require './db/BD.php';
+
+Class Reserva{
+
+    $id;
+    $socio;
+    $instalacion;
+    $fecha;
+    $minutos;
+    $penalizacion;
+
+
+    public __construct($socio,$instalacion,$fecha,$minutos,$penalizacion=null,$id=null){
+        
+        this->socio=$socio;
+        this->instalacion=$instalacion;
+        this->fecha=$fecha;
+        this->minutos=$minutos;
+        this->penalizacion=$penalizacion;
+        this->id=$id;
+
+    }
+
+    static function getReserva($id){
+
+    }
+    
+}
+
+
+>
