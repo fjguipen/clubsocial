b639dd08543f8823dd85a0e185cbb70a64522be4
fjguipen <fjguipen@gmail.com>
2019-02-14 14:49:54 +0100

    html squeleton

--- views/estadisticas.php
+++ views/estadisticas.php
@@ -1,8 +1,26 @@
 <?php session_start(); 
 
-if (isset($_SESSION["user"])){
-   print_r($_SESSION["user"]["username"]);
-} else {
+if (isset($_SESSION["user"])){ ?>
+
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+    <title>Estadísticas</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+
+    <body>
+    <header>
+            <?php include("./views/partials/mainmenu.php"); ?>
+            <h1>Estadísticas</h1>
+        </header>
+    <div class="container">
+        
+    </div>
+    </body>
+    </html>
+
+<?php } else {
     include ("login.php");
 }
 ?>
