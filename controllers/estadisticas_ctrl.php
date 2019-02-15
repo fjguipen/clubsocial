74b71302e90fb105a2b1274bb402a70dca390c8c
AdrianHurtadoDeMendoza <adrian.hurmenfer@gmail.com>
2019-02-15 18:53:53 +0100

    commit adri

--- controllers/estadisticas_ctrl.php
+++ controllers/estadisticas_ctrl.php
@@ -1,6 +1,22 @@
 <?
+require './db/BD.php';
  Class Estadisticas_ctrl{
-    
+    private $reservas=new array();
 
- }
+    function __construct($objReserva){
+        $this->reservas=$objReserva;
+    }
+
+    static function generarEstadisticas($instalaciones){ 
+        //
+       foreach($instalaciones as $instalacion){
+            //sacas id instalacion y lom mete dentro array
+            //sacas nombre instalacion y lommm mete array
+
+            $sentencia="SELECT COUNT(*) FROM reservas
+                        WHERE id=$instalacion;";
+           /*array*/ $db ->query($sentencia);
+
+       }
+    }
 ?>
