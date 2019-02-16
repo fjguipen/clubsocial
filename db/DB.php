8d7333ca073420f4a5b2827692889326b098de60
fjguipen <fjguipen@gmail.com>
2019-02-16 10:54:51 +0100

    Clase estática fix

--- db/DB.php
+++ db/DB.php
@@ -1,15 +1,15 @@
 <? 
 
 Class DB{
-    private $server = 'localhost';
-    private $dbName = 'club_social_eusa';
-    private $user = 'root';
-    private $password = '';
+    private static $server = 'localhost';
+    private static $dbName = 'club_social_eusa';
+    private static $user = 'root';
+    private static $password = '';
 
     //Conecta y cierra con la base de datos
     static function query($sentencia){
         //Abro Conexion
-        $conexion=mysqli_connect($server, $user, $password, $dbName);
+        $conexion=mysqli_connect(self::$server, self::$user, self::$password, self::$dbName);
         //ejecuto sentencia MariaDB
         mysqli_query( $conexion,$sentencia);
         //Cierro Conexion
