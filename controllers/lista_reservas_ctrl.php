319dccda8f90ab0e5e9ffac23a9da8cf788390c1
fjguipen <fjguipen@gmail.com>
2019-02-19 19:44:46 +0100

    .

--- controllers/lista_reservas_ctrl.php
+++ controllers/lista_reservas_ctrl.php
@@ -1,14 +1,12 @@
 <?php
 //Requerimiento de acceso a datos usuario.php y reserva.php.
-require($_SERVER['DOCUMENT_ROOT']."/reservas/models/reserva.php");
+require('./reservas/models/reserva.php');
+require('./reservas/models/usuario.php');
 	
-//Validación del método POST.
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$reserva = Array();
-	//Guardamos en el Array todas las reservas llamando al método de Reserva.
-	$reserva->getReserva();
-
-	//Generamos la penalización correspondiente llamando al método de Reserva.
-	$reserva->añadirPenalizacion($idReserva);
+if (isset($_SESSION["user"])){
+	include("./views/lista_reservas.php");
+} else {
+	include("login.php");
 }
+
 ?>
