32518ea7d6b0c32b37aa14e0fae71efd05a4d291
enriberciano <enri_loscaminantes@hotmail.com>
2019-02-19 19:02:32 +0100

    Cambio login

--- views/login.php
+++ views/login.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<title>Iniciar sesión como administrador</title>
+<title>Administrador</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
 <header>
+    <div class="container">
+        <div id="branding">
         <?php include("./views/partials/mainmenu.php"); ?>
-    </header>
+          <img class="logo" alt="logo" src="./assets/img/logo.png"/>
+          <h1><span class="highlight">CLUB SOCIAL EUSA </span></h1>
+        </div>
+    </div>
+</header>
 <div class="container">
+    <br><br><br><br><br>
     <form action="./controllers/admin_ctrl.php?location=<?php echo $_SERVER['REQUEST_URI']  ?>" method="POST">
         <div class="form-group">
             <label for="username">Nombre de usuario:</label>
