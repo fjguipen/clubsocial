8a2cb7a2b238f221ab8faac8f18695136613dd22
AlbertoRodr <alberto.r.cdc67@gmail.com>
2019-02-26 20:17:51 +0100

    Ajuste tablas

--- views/lista_reservas.php
+++ views/lista_reservas.php
@@ -19,49 +19,51 @@
         <!--Comienzo de la tabla-->
     <div class="container">
         <!--Planificación de fila 1 para tabla 1-->
-        <div class="row_table">
-            <!--Fila 1 con datos por columnas => Nº Reserva | Nº Socio | Tipo de Reserva | Fecha | Hora-->
-            <div class="col-md-2"> Nº Reserva </div>
-            <div class="col-md-2"> Nº Socio </div>
-            <div class="col-md-2"> Instalacion </div>
-            <div class="col-md-2"> Fecha </div>
-            <div class="col-md-2"> Hora </div>
-        </div>
-            <?php
-
-                //El controlador ya ha cargado $reservas con los datos               
-                //$reservas = Reserva::getReservas();
-
-            //Recorrido y obtención de datos de las reservas del socio correspondiente:
-            foreach($reservas as $reserva){?>
-            <!--Planificación para el resto de filas tabla1-->
-            <div class="row_table">   
-                <!--Obtención de datos del socio correspondiente
-
-                *******************************************************************
-                *******************************************************************
-                        VER CLASE RESERVA Y COMPROBAR SUS ATRIBUTOS,
-                        LOS QUE AQUI SE LLAMAN NO COINCIDEN CON LOS DE LA
-                        CLASE RESERVA
-                *******************************************************************
-                *******************************************************************        
-
-
-                Nº Reserva | Nº Socio | Tipo de Instalación | Fecha | Hora -->
-                    <div class="col-md-2"> <?php echo $reserva->id?></div>
-                    <div class="col-md-2"> <?php echo $reserva->socio->id?></div>
-                    <div class="col-md-2"> <?php echo $reserva->instalacion->id?></div>
-                    <div class="col-md-2"> <?php echo $reserva->fecha?></div>
-                    <div class="col-md-2"> <?php echo $reserva->minutos?></div>
-            <?php } ?>
+        <div class="tabla">
+            <div class="row_table">
+                <!--Fila 1 con datos por columnas => Nº Reserva | Nº Socio | Tipo de Reserva | Fecha | Hora-->
+                <div class="col-md-2"> Nº Reserva </div>
+                <div class="col-md-2"> Nº Socio </div>
+                <div class="col-md-2"> Instalacion </div>
+                <div class="col-md-2"> Fecha </div>
+                <div class="col-md-2"> Hora </div>
             </div>
-    <!--Planificación de filas para tabla 2-->
-    <div class="row_table">
+                <?php
+
+                    //El controlador ya ha cargado $reservas con los datos               
+                    //$reservas = Reserva::getReservas();
+
+                //Recorrido y obtención de datos de las reservas del socio correspondiente:
+                foreach($reservas as $reserva){?>
+                <!--Planificación para el resto de filas tabla1-->
+                <div class="row_table">   
+                    <!--Obtención de datos del socio correspondiente
+
+                    *******************************************************************
+                    *******************************************************************
+                            VER CLASE RESERVA Y COMPROBAR SUS ATRIBUTOS,
+                            LOS QUE AQUI SE LLAMAN NO COINCIDEN CON LOS DE LA
+                            CLASE RESERVA
+                    *******************************************************************
+                    *******************************************************************        
+
+
+                    Nº Reserva | Nº Socio | Tipo de Instalación | Fecha | Hora -->
+                        <div class="col-md-2"> <?php echo $reserva->id?></div>
+                        <div class="col-md-2"> <?php echo $reserva->socio->id?></div>
+                        <div class="col-md-2"> <?php echo $reserva->instalacion->id?></div>
+                        <div class="col-md-2"> <?php echo $reserva->fecha?></div>
+                        <div class="col-md-2"> <?php echo $reserva->minutos?></div>
+                <?php } ?>
+                </div>
+        </div>
+        <!--Planificación de filas para tabla 2-->
+        <div class="row_table">
         <!---->
         <div class="col-md-4"> Nº de Socio </div>
         <div class="col-md-4"> Nombre y Apellido </div>
         <div class="col-md-4"> Nº Total de Reservas Mensuales </div>
-    </div>
+        </div>
         <?php 
 
             //En el controlador se carga una variable $socios, con los valores necesarios para mostrar
@@ -79,11 +81,11 @@
             <div class="row_table">  
                 <!--Obtención de datos del socio correspondiente
                 Nº Socio | Nombre de Socio | Apellido de Socio | Nº total de Reservas Mensuales -->
-                    <div class="col-md-2"> <?php echo $socio[0]?></div>
-                    <div class="col-md-2"> <?php echo $socio[1]?></div>
-                    <div class="col-md-2"> <?php echo $socio[2]?></div>               
+                    <div class="col-md-4"> <?php echo $socio[0]?></div>
+                    <div class="col-md-4"> <?php echo $socio[1]?></div>
+                    <div class="col-md-4"> <?php echo $socio[2]?></div>               
             <?php } ?>
             </div>
-</div>
+    </div>
 </body>
 </html>
