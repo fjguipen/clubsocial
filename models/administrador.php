11e0db095b4cf4f3d5535d8e233997312a7f8e18
salvaoo <salvadorgilrosales@gmail.com>
2019-02-26 13:40:56 +0100

    arreglo

--- models/administrador.php
+++ models/administrador.php
@@ -9,14 +9,17 @@ class Administrador {
     private $password;
 
     function __construct($email, $password) {
+
         $this->email = $email;
         $this->password = $password;
+        
     }
 
     public function isAdmin() {
         
         $sentencia = "SELECT * FROM administradores WHERE email='$this->email' AND password='$this->password'";
         $result = DB::query($sentencia);
+
         return $result;
     }
 
