aa1a6d23813791abb9ffe5a70182bf3bab5c7689
salvador gil rosales <salvadorgilrosales@outlook.es>
2019-02-27 16:31:16 +0100

    toma perro de javi

--- views/lista_reservas.php
+++ views/lista_reservas.php
@@ -86,10 +86,14 @@
                     <div class="col-md-3 col-center"> <?php echo $socio[0]?></div>
                     <div class="col-md-3 col-center"> <?php echo $socio[1]?></div>
                     <div class="col-md-3 col-center"> <?php echo $socio[2]?></div> 
-                    <form class="form-group col-md-3 col-center-boton" action="./controllers/factura_ctrl.php" method="POST">
-                        <input type="submit" class="form-control col-center-boton" value="Generar">
-                        <input name="nSocio" type="text" value="<?php echo $socio[0]?>" hidden>                     
-                    </form> 
+                    <?php 
+                        if (socio[2] != 0) { ?>
+                            <form onsubmit="getFactura(event)" class="form-group col-md-3 col-center-boton" action="./controllers/factura_ctrl.php" method="POST">
+                                <input type="submit" class="form-control col-center-boton" value="Generar">
+                                <input name="nSocio" type="text" value="<?php echo $socio[0]?>" hidden>                     
+                            </form> 
+                        <?php } 
+                    ?>
             </div>             
             <?php } ?>
         
